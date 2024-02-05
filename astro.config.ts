282ee0d7e17d35642c77f1c98bcf53f8a73b7f50
imsfc <imsfc9@gmail.com>
2024-02-05 21:16:28 +0800

    chore: add unocss dependency

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-
 import sitemap from '@astrojs/sitemap'
+import unocss from 'unocss/astro'
 
 export default defineConfig({
   site: 'https://imsfc.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    unocss({
+      injectReset: true,
+    }),
+  ],
 })
